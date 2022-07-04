cb06cf2d17d3fbfe13758cbbcfe47cf2f7e55ad7
bianling <1343881168@qq.com>
2022-07-04 19:12:33 +0800

    axios

---
+++ src/1.钩子.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Son v-if="isShow" />
+    <button @click="isShow = !isShow">点击销毁</button>
+  </div>
+</template>
+
+<script>
+import Son from "./components/Son.vue";
+export default {
+  name: "App",
+  data() {
+    return {
+      isShow: true,
+    };
+  },
+  components: {
+    Son,
+  },
+};
+</script>
+
+<style>
+</style>
