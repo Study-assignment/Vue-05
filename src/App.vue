75b3c219e87ca5191a5d8db6117a887e82e282c5
bianling <1343881168@qq.com>
2022-07-04 18:57:58 +0800

    完成了生命周期钩子使用练习

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
-  <div></div>
+  <div>
+    <Son v-if="isShow" />
+    <button @click="isShow = !isShow">点击销毁</button>
+  </div>
 </template>
 
 <script>
+import Son from "./components/Son.vue";
 export default {
   name: "App",
+  data() {
+    return {
+      isShow: true,
+    };
+  },
+  components: {
+    Son,
+  },
 };
 </script>
 
---
+++ src/components/Son.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <button @click="fn" ref="btn">点击获取sum</button>
+    <button @click="sum++">点击sum++{{ sum }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sum: 0,
+      timer: null,
+    };
+  },
+  methods: {
+    fn() {
+      console.log(this.sum);
+    },
+  },
+  beforeCreate() {
+    console.log(this.sum);
+  },
+  created() {
+    console.log(this.sum);
+  },
+  beforeMount() {
+    console.log(this.$refs.btn);
+  },
+  mounted() {
+    this.timer = setInterval(() => {
+      console.log(1);
+    }, 1000);
+    console.log(this.$refs.btn);
+  },
+  beforeUpdate() {
+    console.log(this.sum);
+  },
+  updated() {
+    console.log(this.sum);
+  },
+  beforeDestroy() {
+    clearInterval(this.timer);
+  },
+  destroyed() {
+    console.log(this);
+  },
+};
+</script>
+
+<style>
+</style>
